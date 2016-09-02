eff57e37d9ffcaca62805d64282011896d21a109
Sanders Gutiérrez <ing.sanders@gmail.com>
2016-09-02 01:03:01 -0500

    Instalación de Nodemon para vigilar los cambios en los archivos y restaurar el servidor una vez guardados los cambios.

--- servidor.js
+++ servidor.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-    response.write("Bienvenidos a mi sitio");
+    response.write("Bienvenidos a mi sitio me llamo Sanders Gutiérrez");
     response.end();
 });
 
