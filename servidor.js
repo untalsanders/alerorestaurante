558df93fc21d69c310fda0d474f333928675d1d0
Sanders Gutiérrez <ing.sanders@gmail.com>
2016-09-08 17:48:32 -0500

    -> Desinstalado Express JS, pug y jade

--- servidor.js
+++ servidor.js
@@ -1,35 +1,37 @@
-// const http = require('http');
-// const fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    const TEMPLATE_DIR = './public/';
+    switch(request.url) {
+        case '/':
+            template = 'index.html';
+            break;
+        case '/acercade':
+            template = 'acercade.html';
+            break;
+        default:
+            template = '404.html';
+    }
+    fs.readFile(TEMPLATE_DIR + template, (err, data) => {
+        response.write(data);
+        response.end();
+    });
+});
 //
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-//     const TEMPLATE_DIR = './templates/';
-//     switch(request.url) {
-//         case '/':
-//             template = 'index.html';
-//             break;
-//         case '/acercade':
-//             template = 'acercade.html';
-//             break;
-//         default:
-//             template = '404.html';
-//     }
-//     fs.readFile(TEMPLATE_DIR + template, (err, data) => {
-//         response.write(data);
-//         response.end();
-//     });
+server.listen(3000,'localhost');
+
+// const express = require('express');
+// const fs = require('fs');
+// const app = express();
+// app.use(express.static(__dirname + '/public'));
+//
+// app.get('/', (request, response) => {
+//     // response.render('index.jade');
+//     console.log(request.url);
 // });
 //
-// server.listen(3000,'localhost');
-
-const express = require('express');
-const app = express();
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (request, response) => {
-    response.sendFile('index.html');
-});
-
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
-});
+// app.listen(3000, () => {
+//     console.log('Servidor corriendo en http://localhost:3000');
+// });
