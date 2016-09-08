6f8a21cfb10b501a860afd4a739376ee7bfa2aa1
Sanders Gutiérrez <ing.sanders@gmail.com>
2016-09-08 03:32:08 -0500

    -> Definiendo un servidor node básico con expressjs -> Crea la ruta / y da como respuesta a la petición el mensaje "Hola Mundo con ExpressJS"

--- servidor.js
+++ servidor.js
@@ -1,23 +1,34 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+//
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+//     const TEMPLATE_DIR = './templates/';
+//     switch(request.url) {
+//         case '/':
+//             template = 'index.html';
+//             break;
+//         case '/acercade':
+//             template = 'acercade.html';
+//             break;
+//         default:
+//             template = '404.html';
+//     }
+//     fs.readFile(TEMPLATE_DIR + template, (err, data) => {
+//         response.write(data);
+//         response.end();
+//     });
+// });
+//
+// server.listen(3000,'localhost');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    const TEMPLATE_DIR = './templates/';
-    switch(request.url) {
-        case '/':
-            template = 'index.html';
-            break;
-        case '/acercade':
-            template = 'acercade.html';
-            break;
-        default:
-            template = '404.html';
-    }
-    fs.readFile(TEMPLATE_DIR + template, (err, data) => {
-        response.write(data);
-        response.end();
-    });
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hola Mundo con Express JS');
 });
 
-server.listen(3000,'localhost');
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
