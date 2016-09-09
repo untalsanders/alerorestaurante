b6b2dc8a6a3026a81fb3c51e6e55b24f03b9a010
Sanders Gutiérrez <ing.sanders@gmail.com>
2016-09-09 18:11:07 -0500

    --> Gestión de plantillas con el motor de plantillas pugjs 	--> Definición del directorio por defecto de plantillas: [app.set('views', './views')] 	--> Definición del motor de plantillas: [app.set('view engine', 'pug')]
    --> Render de una plantilla con el motor pugjs [res.render('index.pug')]

--- servidor.js
+++ servidor.js
@@ -2,10 +2,16 @@ const express = require('express');
 const fs = require('fs');
 
 var app = express();
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+// Establece carpeta por defecto para las vistas
+app.set('views', './views');
+// Establece carpeta por defecto para las vistas
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    // res.sendFile('index.html');
+    // res.render('index', {title: 'Sanders Gutiérrez', message: 'Bienvenidos a mi web'});
+    res.render('index.pug');
 });
 
 app.listen(3000, 'localhost', () => {
