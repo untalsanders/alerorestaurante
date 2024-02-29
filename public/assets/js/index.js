1774627d7c36e364d4f6c3b7d6c03ce246b0e651
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-02-29 16:23:38 -0300

    chore: remove code which not needed
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,33 +1,5 @@
 'use strict'
 
-const reportPermissionStatus = (state) => console.log(`Permission: ${state}`)
-
-const cbPermission = navigator.permissions.query({ name: 'clipboard-read' })
-console.log(cbPermission)
-
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve()
-    }, 300)
+document.addEventListener('DOMContentLoaded', async event => {
+    console.log('DOM fully loaded and parsed')
 })
-
-const handleFulfilledA = () => console.log('HandleFulfilledA')
-const handleRejectedA = () => console.log('HandleRejectedA')
-
-myPromise.then(handleFulfilledA).then(handleFulfilledA).catch(handleRejectedA)
-
-// navigator.permissions.query({ name: 'clipboard-read' }).then(result => {
-//     if (result.state == 'granted') {
-//         reportPermissionStatus(result.state)
-//         navigator.clipboard.readText().then(clipText => {
-//             const cb = document.createElement('div')
-//             cb.style.backgroundColor = 'red'
-//             cb.style.color = 'white'
-//             cb.style.padding = '20px'
-//             const newContent = document.createTextNode(clipText)
-//             cb.appendChild(newContent)
-//             const mainContent = document.querySelector('.principal')
-//             mainContent.insertAdjacentElement('afterbegin', cb)
-//         })
-//     }
-// })
