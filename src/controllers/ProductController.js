e7a273d0f1fb2f02e0ffc9f677347fe5564e36dd
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-03-03 18:34:08 -0300

    chore: add semantic variables
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,42 +2,47 @@
 
 const products = [
     {
-        "id": 1,
-        "title": "Narana",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "image": "camiseta1.png"
+        id: 1,
+        title: 'Narana',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: 'camiseta1.png',
     },
     {
-        "id": 2,
-        "title": "Roja",
-        "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        "image": "camiseta2.png"
+        id: 2,
+        title: 'Roja',
+        description:
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        image: 'camiseta2.png',
     },
     {
-        "id": 3,
-        "title": "Azul",
-        "description": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-        "image": "camiseta3.png"
+        id: 3,
+        title: 'Azul',
+        description:
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        image: 'camiseta3.png',
     },
     {
-        "id": 4,
-        "title": "Verde",
-        "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "image": "camiseta4.png"
-    }
+        id: 4,
+        title: 'Verde',
+        description:
+            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        image: 'camiseta4.png',
+    },
 ]
 
 const getProducts = (req, res) => {
     res.render('products.twig', {
-        products
+        products,
     })
 }
 
 const getProduct = (req, res) => {
     const { id } = req.params
-    products.filter(product => (id == product.id ? res.render('product-details.twig', { product }) : null))
+    products.filter((product) => (id == product.id ? res.render('product-details.twig', { product }) : null))
 }
 
 module.exports = {
-    getProducts, getProduct
+    getProducts,
+    getProduct,
 }
